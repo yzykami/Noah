c74cbabf4db3b16a12966ca862051c33fa6423de
yzykami <yzykami@sina.com>
2017-07-17 17:30:10 +0800

    xiaopan.sketchsample漏传

---
+++ app/src/main/java/me/xiaopan/sketchsample/fragment/OtherTestFragment.java
@@ -0,0 +1,10 @@
+package me.xiaopan.sketchsample.fragment;
+
+import me.xiaopan.sketchsample.BaseFragment;
+import me.xiaopan.sketchsample.BindContentView;
+import com.tzw.noah.R;
+
+@BindContentView(R.layout.fragment_other_test)
+public class OtherTestFragment extends BaseFragment {
+
+}
