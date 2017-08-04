02cd5f0b8f47dd0e5b337b4f5dc9b728b7c257d7
yzykami <yzykami@sina.com>
2017-08-04 10:53:27 +0800

    修改聊天气泡

---
+++ app/src/main/java/com/tzw/noah/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.tzw.noah;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tzw.noah.ui.MyBaseActivity;
+
+/**
+ * Created by yzy on 2017/7/28.
+ */
+
+public class WelcomeActivity extends MyBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
